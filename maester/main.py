deb2e472160804f40a8538b795c856d7106faf51
Yosoi <yosoi@protonmail.com>
2020-03-26 02:37:11 +0000

    .env and .gitignore

--- maester/main.py
+++ maester/main.py
@@ -1,14 +1,32 @@
 import boto3
 import discord
+from dotenv import load_dotenv
+import os
 
-client = boto3.client("lex-runtime")
 
-response = client.post_text(
-	botName = "Maester",
-	botAlias = "maester",
-	userId = "dev", # replace this with some kind of id from discord
-	inputText = "roll"
-)
+load_dotenv()
 
-print("Greetings, my liege.")
+
+TOKEN = os.getenv("TOKEN")
+BOT = os.getenv("BOT")
+ALIAS = os.getenv("ALIAS")
+
+
+def get_lex_response(lex_client, bot_name, bot_alias, user_id, input_text):
+	return lex_client.post_text(
+		botName = bot_name,
+		botAlias = bot_alias,
+		userId = user_id,
+		inputText = input_text
+	)
+
+
+lex = boto3.client("lex-runtime")
+
+
+user_id = "dev"
+message = "roll"
+response = get_lex_response(lex, BOT, ALIAS, user_id, message)
+
+
 print(response["message"])
