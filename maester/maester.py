e2a3e12713b19a0b3c8fc71fa009b2e4e06015a6
Yosoi <yosoi@protonmail.com>
2020-03-29 16:10:46 +0000

    improve dice notation parsing

--- maester/maester.py
+++ maester/maester.py
@@ -1,6 +1,7 @@
 import boto3
 import discord
 import random
+import re
 
 
 class MaesterClient(discord.Client):
@@ -18,11 +19,20 @@ class MaesterClient(discord.Client):
 		if message.author == self.user:
 			return
 		if message.channel.type is discord.ChannelType.private:
-			response = self.get_lex_response(str(message.author.id), message.content)
+			content = self.preprocess_message_content(message.content)
+			response = self.get_lex_response(str(message.author.id), content)
 			await message.channel.send(response["message"])
 		elif message.content.lower().startswith("maester"):
 			response = random.choice(self.pm_only_messages)
 			await message.channel.send(response)
+
+	def preprocess_message_content(self, content):
+		dice_notation_regex = r"\d+d\d+"
+		matches = re.findall(dice_notation_regex, content, flags=re.IGNORECASE)
+		processed_matches = [" d".join(re.split("d", match, flags=re.IGNORECASE)) for match in matches]
+		for i in range(len(matches)):
+			content = content.replace(matches[i], processed_matches[i])
+		return content
 
 	def get_lex_response(self, user_id, input_text):
 		return self.lex_client.post_text(
